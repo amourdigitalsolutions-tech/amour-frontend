368e95f64d9ce814119e5a6217a289b5bf356117
Aaron Teshale <aaronteshale138@gmail.com>
2026-07-24 18:08:57 +0300

    feat: add signup route to main application router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import ChatRoom from './pages/ChatRoom';
 
@@ -62,6 +63,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/chat" element={<ChatRoom />} />
         <Route path="*" element={<Navigate to="/" replace />} />
